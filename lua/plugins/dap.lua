7437a72537acec97c4a5a64989edab2ed7786301
theRadn <rkurniawanagung32@gmail.com>
2025-08-05 20:23:44 +0700

    Change step out and step over keybind

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,5 +1,21 @@
 return {
   "mfussenegger/nvim-dap",
+  keys = {
+    {
+      "<leader>dO",
+      function()
+        require("dap").step_out()
+      end,
+      desc = "Step Out",
+    },
+    {
+      "<leader>do",
+      function()
+        require("dap").step_over()
+      end,
+      desc = "Step Over",
+    },
+  },
   config = function()
     local dap = require("dap")
     dap.adapters.cppdbg = {
