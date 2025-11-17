0f0228da59e267c7162d5154eb81e293749c74b1
theRadn <rkurniawanagung32@gmail.com>
2025-11-17 22:28:52 +0700

    Add always hidden files option based on file extension in oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,6 +5,14 @@ return {
   opts = {
     view_options = {
       show_hidden = true,
+      is_always_hidden = function(name, bufnr)
+        local hidden_extensions = { ".class", ".exe", ".o", ".jar" }
+        for _, ext in ipairs(hidden_extensions) do
+          if vim.endswith(name, ext) then
+            return true
+          end
+        end
+      end,
     },
     skip_confirm_for_simple_edits = true,
   },
