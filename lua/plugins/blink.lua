aff75496b3da01bb58ccca46fd3d30c567fe39d5
theRadn <rkurniawanagung32@gmail.com>
2026-01-15 20:15:53 +0700

    Add blink keymap options

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -14,8 +14,16 @@ return {
       menu = {
         draw = {
           columns = {
+            { "item_idx", gap = 1 },
             { "label", "label_description", gap = 1 },
             { "kind_icon", "kind", "source_name", gap = 1 },
+          },
+          components = {
+            item_idx = {
+              text = function(ctx)
+                return ctx.idx == 10 and "0" or ctx.idx >= 10 and " " or tostring(ctx.idx)
+              end,
+            },
           },
         },
       },
@@ -23,6 +31,18 @@ return {
       trigger = {
         show_on_blocked_trigger_characters = { " ", "\n", "\t", ";" },
       },
+    },
+    keymap = {
+      ["<A-1>"] = { function(cmp) cmp.accept({ index = 1 }) end, },
+      ["<A-2>"] = { function(cmp) cmp.accept({ index = 2 }) end, },
+      ["<A-3>"] = { function(cmp) cmp.accept({ index = 3 }) end, },
+      ["<A-4>"] = { function(cmp) cmp.accept({ index = 4 }) end, },
+      ["<A-5>"] = { function(cmp) cmp.accept({ index = 5 }) end, },
+      ["<A-6>"] = { function(cmp) cmp.accept({ index = 6 }) end, },
+      ["<A-7>"] = { function(cmp) cmp.accept({ index = 7 }) end, },
+      ["<A-8>"] = { function(cmp) cmp.accept({ index = 8 }) end, },
+      ["<A-9>"] = { function(cmp) cmp.accept({ index = 9 }) end, },
+      ["<A-0>"] = { function(cmp) cmp.accept({ index = 10 }) end, },
     },
     snippets = {
       preset = "luasnip",
@@ -33,6 +53,16 @@ return {
       keymap = {
         ["<Down>"] = { "select_next", "fallback" },
         ["<Up>"] = { "select_prev", "fallback" },
+        ["<A-1>"] = { function(cmp) cmp.accept({ index = 1 }) end, },
+        ["<A-2>"] = { function(cmp) cmp.accept({ index = 2 }) end, },
+        ["<A-3>"] = { function(cmp) cmp.accept({ index = 3 }) end, },
+        ["<A-4>"] = { function(cmp) cmp.accept({ index = 4 }) end, },
+        ["<A-5>"] = { function(cmp) cmp.accept({ index = 5 }) end, },
+        ["<A-6>"] = { function(cmp) cmp.accept({ index = 6 }) end, },
+        ["<A-7>"] = { function(cmp) cmp.accept({ index = 7 }) end, },
+        ["<A-8>"] = { function(cmp) cmp.accept({ index = 8 }) end, },
+        ["<A-9>"] = { function(cmp) cmp.accept({ index = 9 }) end, },
+        ["<A-0>"] = { function(cmp) cmp.accept({ index = 10 }) end, },
       },
     },
     sources = {
@@ -42,7 +72,7 @@ return {
           -- score_offset = 100,
           override = {
             get_trigger_characters = function()
-              return { "#", ";", "@", "!", "." }
+              return { "#", "." }
             end,
           },
         },
