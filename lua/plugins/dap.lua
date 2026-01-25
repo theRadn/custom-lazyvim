4573dd6052f58484633de1361620b485918ab050
theRadn <rkurniawanagung32@gmail.com>
2026-01-25 19:22:29 +0700

    Change DAP configuration to use from LazyExtras

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -16,48 +16,48 @@ return {
       desc = "Step Over",
     },
   },
-  config = function()
-    local dap = require("dap")
-    dap.adapters.cppdbg = {
-      id = "cppdbg",
-      type = "executable",
-      command = vim.fn.expand("~/.local/share/nvim/mason/bin/OpenDebugAD7"),
-    }
-    dap.configurations.cpp = {
-      {
-        name = "Launch file",
-        type = "cppdbg",
-        request = "launch",
-        -- program = function()
-        --   return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        -- end,
-        program = vim.fn.expand("%:t:r"),
-        cwd = "${workspaceFolder}",
-        stopAtEntry = true,
-      },
-    }
-    dap.configurations.c = dap.configurations.cpp
-    dap.configurations.rust = dap.configurations.cpp
-
-    dap.adapters["pwa-node"] = {
-      type = "server",
-      host = "127.0.0.1",
-      port = 9229,
-      executable = {
-        command = "js-debug-adapter",
-        args = {
-          "9229",
-        },
-      },
-    }
-    dap.configurations.javascript = {
-      {
-        type = "pwa-node",
-        request = "launch",
-        name = "Launch file",
-        program = "${file}",
-        cwd = "${workspaceFolder}",
-      },
-    }
-  end,
+  -- config = function()
+  --   local dap = require("dap")
+  --   -- dap.adapters.cppdbg = {
+  --   --   id = "cppdbg",
+  --   --   type = "executable",
+  --   --   command = vim.fn.expand("~/.local/share/nvim/mason/bin/OpenDebugAD7"),
+  --   -- }
+  --   -- dap.configurations.cpp = {
+  --   --   {
+  --   --     name = "Launch file",
+  --   --     type = "cppdbg",
+  --   --     request = "launch",
+  --   --     -- program = function()
+  --   --     --   return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+  --   --     -- end,
+  --   --     program = vim.fn.expand("%:t:r"),
+  --   --     cwd = "${workspaceFolder}",
+  --   --     stopAtEntry = true,
+  --   --   },
+  --   -- }
+  --   -- dap.configurations.c = dap.configurations.cpp
+  --   -- dap.configurations.rust = dap.configurations.cpp
+  --
+  --   dap.adapters["pwa-node"] = {
+  --     type = "server",
+  --     host = "127.0.0.1",
+  --     port = 9229,
+  --     executable = {
+  --       command = "js-debug-adapter",
+  --       args = {
+  --         "9229",
+  --       },
+  --     },
+  --   }
+  --   dap.configurations.javascript = {
+  --     {
+  --       type = "pwa-node",
+  --       request = "launch",
+  --       name = "Launch file",
+  --       program = "${file}",
+  --       cwd = "${workspaceFolder}",
+  --     },
+  --   }
+  -- end,
 }
