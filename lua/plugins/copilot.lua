e391ade8467aa21292322c06169c7d0bccb08d5a
Raden Kurniawan Agung Fitrianto <rkurniawanagung32@gmail.com>
2026-05-04 11:24:03 +0700

    Add copilot chat & copilot-lsp for NES

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,15 +1,20 @@
 return {
   "zbirenbaum/copilot.lua",
+  dependencies = {
+    "copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
+  },
+  cmd = "Copilot",
+  event = "InsertEnter",
   opts = {
     suggestion = {
       auto_trigger = true,
     },
     server_opts_overrides = {
-        settings = {
-            telemetry = {
-                telemetryLevel = "off",
-            },
+      settings = {
+        telemetry = {
+          telemetryLevel = "off",
         },
-    }, 
+      },
+    },
   },
 }
