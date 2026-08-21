298508aba71b17706e5c98e7b1282c0fc79bf423
Raden Kurniawan Agung Fitrianto <rkurniawanagung32@gmail.com>
2026-08-21 07:28:52 +0700

    Reduce smooth scroll duration

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,7 +4,7 @@ return {
     scroll = {
       enabled = true,
       animate = {
-        duration = { step = 10, total = 100 },
+        duration = { step = 10, total = 50 },
         easing = "linear",
       },
     },
